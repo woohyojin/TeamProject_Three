26bccee2c7ad643308990310b2b770c58032b7fb
KangDae <eownsl119@gmail.com>
2022-05-18 20:16:22 +0900

    sign up

--- app/src/main/java/com/example/othercock/ui/Login/SignupFragment.java
+++ app/src/main/java/com/example/othercock/ui/Login/SignupFragment.java
@@ -1,11 +1,14 @@
 package com.example.othercock.ui.Login;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -14,11 +17,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.othercock.R;
+import com.example.othercock.Socket.Protocol;
+import com.example.othercock.Socket.service_Socket;
 
 import java.util.HashMap;
 
 public final class SignupFragment extends Fragment {
-    private HashMap _$_findViewCache;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +32,40 @@ public final class SignupFragment extends Fragment {
 
         ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
 
+
+
+
         if (actionBar != null) {
             actionBar.setDisplayShowTitleEnabled(false);
         }
+
+        Button but = (Button)getView().findViewById(R.id.button_signin);
+        EditText editEmail = (EditText)getView().findViewById(R.id.et_email);
+        EditText editUser = (EditText)getView().findViewById(R.id.et_username);
+        EditText editPassword = (EditText)getView().findViewById(R.id.et_password);
+        EditText editPhone = (EditText)getView().findViewById(R.id.et_phone);
+        EditText editconfirmPassword = (EditText)getView().findViewById(R.id.et_confirm_password);
+
+
+
+                but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(editPassword.getText().toString().equals(editconfirmPassword.getText().toString())){
+                    Intent intent = new Intent(getContext(), service_Socket.class);
+                    intent.putExtra("pw", Protocol.REGISTER +"|"+ editEmail.getText().toString() + "|" + editUser.getText().toString() +
+                            "|" +editPhone.getText().toString()+"|"+ editPassword.getText().toString());
+                    getActivity().startService(intent);
+                } else{
+                    System.out.println("비밀번호가 일치 하지 않습니다.");
+                }
+
+
+
+
+            }
+        });
 
         toolbar.setNavigationOnClickListener((View.OnClickListener)(new View.OnClickListener() {
             public final void onClick(View it) {
